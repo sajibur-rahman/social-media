cf90e5a7b616c871037ffde37faee49a9a751044
sajibur-rahman <sajiburr21@gmail.com>
2022-04-02 09:23:56 +0600

    project building is start

--- src/App.js
+++ src/App.js
@@ -1,21 +1,9 @@
-import { Route, Routes } from 'react-router-dom';
-import './App.css';
-import Comments from './components/Comments/Comments';
-import Header from './components/Header/Header';
-import Home from './components/Home/Home';
-import Posts from './components/Posts/Posts';
-import UserPosts from './components/UserPosts/UserPosts';
+import './assets/css/global.css';
 
 function App() {
   return (
     <div>
-      <Header/>
-      <Routes>
-        <Route path='/' element={<Home/>}/>
-        <Route path='/posts' element={<Posts/>}/>
-        <Route path='/posts/:userId' element={<UserPosts/>}/>
-        <Route path='/comments/:postId' element={<Comments/>}/>
-      </Routes>
+      <h1>welcome to social world</h1> 
     </div>
   );
 }
