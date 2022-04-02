145cb23b99d98f94c6b1661e952e9e43932eb6d6
sajibur-rahman <sajiburr21@gmail.com>
2022-04-02 16:27:45 +0600

    project is done

--- src/components/Post.js
+++ src/components/Post.js
@@ -4,7 +4,6 @@ import { useNavigate } from 'react-router-dom';
 
 const Post = ({post}) => {
     const navigate =  useNavigate()
-    console.log(post);
     const {title,body,id} = post;
     return (
         <div className='border-2 border-purple-700 rounded-lg p-4 w-2/4 m-7 space-y-3'>
